2b3f3ed95f3b391758fd452e076a2e4ebe753529
Mohit Taneja <mohit@taneja.me>
2017-08-14 22:09:08 -0700

    Initial version of the Tipper app after just watching the video for building the basic version of tipping app.

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipController: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Hide Keyboard when tapped outside of the editing field
+    @IBAction func HideKeyboard(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    // Calculate tip when the bill amount is changed
+    @IBAction func CalculateTip(_ sender: Any) {
+        let billAmount = Double(billField.text!) ?? 0
+        let tipPercentages = [0.18, 0.20, 0.25]
+        let tipValue = billAmount*tipPercentages[tipController.selectedSegmentIndex]
+        
+        tipLabel.text = String.init(format: "$%.2f", tipValue)
+        totalLabel.text = String.init(format: "$%.2f", billAmount + tipValue)
+        
+    }
 }
 
